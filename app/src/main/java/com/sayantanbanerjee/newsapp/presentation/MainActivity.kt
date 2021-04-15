7874050816ee48036672ad00e73531ad07875d64
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2021-04-15 23:39:03 +0530

    Navigation components introduced

--- app/src/main/java/com/sayantanbanerjee/newsapp/presentation/MainActivity.kt
+++ app/src/main/java/com/sayantanbanerjee/newsapp/presentation/MainActivity.kt
@@ -2,11 +2,29 @@ package com.sayantanbanerjee.newsapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.sayantanbanerjee.newsapp.R
+import com.sayantanbanerjee.newsapp.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // setting up the navigation fragment and also linking it to the navigation controller
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bnvNews.setupWithNavController(
+            navController
+        )
     }
 }
